f7d9deaff66a4d4c887287bc38a03983bed54460
neoxr <neoxr@null.ner>
2019-11-29 17:58:00 +0700

    v0.1.1

---
+++ system/color.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+* DARIPADA RECODE SCRIPT MENDING EDIT WARNA WKWKW :V
+* MOTHERFUCKER / BANGSAT :V
+*/
+define("NETRAL", "\e[0m");
+define("KUNING", "\e[38;5;228m");
+define("MERAH", "\e[91m");
+define("HIJAU", "\e[38;5;0;32m");
+define("ST_HITAM", "\e[40;38;5;231m");
